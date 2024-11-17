a3fc5a4afe509f193dbd11253575f4dd5269a878
Leinnan <siuszko@zoho.com>
2024-11-17 08:16:34 +0000

    Build: (2f23d23) Plugin ready for Bevy 0.15

---
+++ simple_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(a: number, b: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_export_3: WebAssembly.Table;
+export function __externref_table_alloc(): number;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h306bd1502fe19c83(a: number, b: number, c: number): void;
+export function closure3001_externref_shim(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h23c090b84ec44cde(a: number, b: number): void;
+export function closure3004_externref_shim(a: number, b: number, c: number, d: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7e759b9017b07232(a: number, b: number): void;
+export function closure105360_externref_shim(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
