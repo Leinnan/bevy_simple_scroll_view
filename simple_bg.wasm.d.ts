600b7e7c3b746e209e13a37d0f90faa6cd8e2fe0
Leinnan <siuszko@zoho.com>
2025-05-07 08:47:56 +0000

    Build: (17d4cfb) 0.4

---
+++ simple_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1c21a3a0f3153cd3: (a: number, b: number, c: number) => void;
+export const closure5035_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he95ed6491b3eede5: (a: number, b: number) => void;
+export const closure5040_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb9c6ea294dc2c419: (a: number, b: number) => void;
+export const closure118797_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
